706344ef936bc3acab3ccc4f7fdd1963aa80d347
sahrulguns <Sahrulgunawan2519@gmail.com>
2023-06-13 15:00:58 +0700

    initian commit

---
+++ index.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+     <meta charset="UTF-8">
+     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+     <title>Document</title>
+</head>
+
+<body>
+     <table border="1" cellpadding="10" cellspacing="0">
+          <tr>
+               <td>NO</td>
+               <td>NRP</td>
+               <td>Nama</td>
+               <td>Email</td>
+               <td>Jurusan</td>
+               <td>Gambar</td>
+          </tr>
+     </table>
+</body>
+
+</html>
